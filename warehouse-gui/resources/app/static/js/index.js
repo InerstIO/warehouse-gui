2ec44c80067d5d335f7616628c2598c7afef2f19
InerstIO <inerstio@gmail.com>
2018-05-26 01:01:02 -0700

    showBatchOrder finished

--- warehouse-gui/resources/app/static/js/index.js
+++ warehouse-gui/resources/app/static/js/index.js
@@ -64,10 +64,41 @@ var sendInput = function() {
 };
 
 function showBatchOrder() {
+    clearMap()
     var orderid = document.getElementById("orderid").value;
-    for (var i in batchresult[orderid]) {
-        var locid = batchresult[orderid][i].Y*39+batchresult[orderid][i].X;
-        var pos = document.getElementsByClassName('item' + locid.toString())[0];
-        pos.style.backgroundColor = "lightblue"
+    for (var i in batchresult[orderid].slice(1)) {
+        var cur = batchresult[orderid][i];
+        var next = batchresult[orderid][parseInt(i)+1];
+        if (next.X == cur.X) {
+            var range = Array.from(new Array(Math.abs(cur.Y-next.Y)+1), (x,i) => i + Math.min(cur.Y,next.Y))
+            for (var j of range) {
+                var locid = j*39+cur.X;
+                var pos = document.getElementsByClassName('item' + locid.toString())[0];
+                pos.style.backgroundColor = "lightblue";
+            }
+        } else if (next.Y == cur.Y) {
+            var range = Array.from(new Array(Math.abs(cur.X-next.X)+1), (x,i) => i + Math.min(cur.X,next.X))
+            for (var j of range) {
+                var locid = cur.Y*39+j;
+                var pos = document.getElementsByClassName('item' + locid.toString())[0];
+                pos.style.backgroundColor = "lightblue";
+            }
+        }
+    }
+}
+
+function clearMap() {
+    for (i=0; i<39; i++) {
+        for (j=0; j<23; j++) {
+            if (i%2*j%2) {
+                var locid = j*39+i;
+                var pos = document.getElementsByClassName('item' + locid.toString())[0];
+                pos.style.backgroundColor = "rgba(251, 219, 121, 1)";
+            } else {
+                var locid = j*39+i;
+                var pos = document.getElementsByClassName('item' + locid.toString())[0];
+                pos.style.backgroundColor = "rgba(255, 255, 255, 1)";
+            }
+        }
     }
 }
